4cdb768a81364d90dc1a702da59417d30552a86e
dazmaks <mysagaz@mail.ru>
2021-04-30 14:09:39 +0300

    remove energy pow

--- seam-carving/src/net/seamcarving/carve/Energy.java
+++ seam-carving/src/net/seamcarving/carve/Energy.java
@@ -2,7 +2,6 @@ package net.seamcarving.carve;
 
 import java.awt.*;
 
-import static java.lang.StrictMath.pow;
 import static java.lang.StrictMath.sqrt;
 
 public class Energy {
@@ -21,8 +20,8 @@ public class Energy {
                 int g = color1.getGreen() - color2.getGreen();
                 int b = color1.getBlue() - color2.getBlue();
 
-                Color ensq = new Color((int)sqrt(pow(r + g + b, 2))); //calculating energy
-                int midensq = (int) ((ensq.getRed() + ensq.getBlue() + ensq.getGreen())/1.5); //calculating arithmetic mean
+                Color ensq = new Color((int)sqrt(r*r + g*g + b*b)); //calculating energy
+                int midensq = ensq.getRed() + ensq.getBlue() + ensq.getGreen(); //calculating arithmetic mean
 
                 Color newColor = new Color(midensq, midensq, midensq); //making image mono-chrome
 
