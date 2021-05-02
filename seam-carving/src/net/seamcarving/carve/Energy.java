b8a7ca539cf2e7cdc4f31a8135db7efb5c0fa0f3
dazmaks <mysagaz@mail.ru>
2021-05-02 14:24:00 +0300

    fix calculation

--- seam-carving/src/net/seamcarving/carve/Energy.java
+++ seam-carving/src/net/seamcarving/carve/Energy.java
@@ -6,19 +6,25 @@ import static java.lang.StrictMath.sqrt;
 
 public class Energy {
     public static void energy(Img img) {
+        Color defaultColor;
+        Color color1, color2;
+
+        int r, g, b;
+
         for (int x = 0; x<img.width; x++){
             for(int y = 0; y<img.height; y++){
                 //getting current pixel color
-                Color color1 = img.rgbColor(x, y);
-                Color color2 = img.rgbColor(x, y);
+                defaultColor = img.rgbColor(x, y);
+                color1 = defaultColor;
+                color2 = defaultColor;
 
                 //out of bounds
                 if (x != 0) color1 = img.rgbColor(x-1, y);
                 if (x != img.width-1) color2 = img.rgbColor(x+1, y);
 
-                int r = color1.getRed() - color2.getRed();
-                int g = color1.getGreen() - color2.getGreen();
-                int b = color1.getBlue() - color2.getBlue();
+                r = (color1.getRed() - color2.getRed())/2;
+                g = (color1.getGreen() - color2.getGreen())/2;
+                b = (color1.getBlue() - color2.getBlue())/2;
 
                 Color ensq = new Color((int)sqrt(r*r + g*g + b*b)); //calculating energy
                 int midensq = ensq.getRed() + ensq.getBlue() + ensq.getGreen(); //calculating arithmetic mean
